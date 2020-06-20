1ee895ddc9f38e1072a132e2a509eb52fd2ae6da
kalyancharlie <kalyancharlie@gmail.com>
2020-06-20 16:37:49 +0530

    Added Interface Definition in Concrete Class

--- src/com/kalyancharlie/ContactService.java
+++ src/com/kalyancharlie/ContactService.java
@@ -1,8 +1,12 @@
 package com.kalyancharlie;
+import java.util.ArrayList;
 
 public interface ContactService {	
 	public void insertContact(Contacts contact, String memory);
-	public void updateContact(Contacts contact, String memory);
+	public void updateContact(Contacts old, Contacts updated, String memory);
 	public Contacts searchContact(String name, String memory);
 	public Contacts searchContact(long mobileNumber, String memory);
+	public void display(ArrayList<Contacts> sim, ArrayList<Contacts> phone);
+	public void copy(Contacts contact, String target );
+	public void copyAll(ArrayList<Contacts> contact, String target);
 }
---
+++ src/com/kalyancharlie/ContactServiceImpl.java
@@ -0,0 +1,142 @@
+package com.kalyancharlie;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ContactServiceImpl implements ContactService {
+	ArrayList<Contacts> sim = new ArrayList<Contacts>();
+	ArrayList<Contacts> phone = new ArrayList<Contacts>();
+	
+	public ContactServiceImpl() {
+		
+	}
+
+	public ArrayList<Contacts> getSim() {
+		return sim;
+	}
+
+	public ArrayList<Contacts> getPhone() {
+		return phone;
+	}
+	
+	@Override
+	public void insertContact(Contacts contact, String memory) {
+		if(memory.toLowerCase().equals("sim")) {
+			sim.add(contact);
+		} else if (memory.toLowerCase().equals("phone")) {
+			phone.add(contact);
+		}
+	}
+
+	@Override
+	public void updateContact(Contacts old, Contacts updated, String memory) {
+		Contacts found = searchContact(old.name, memory);
+		if(memory.toLowerCase().equals("sim")) {
+			for(int i=0; i<sim.size(); i++) {
+				if(sim.get(i).equals(found)) {
+					sim.set(i, updated);
+					System.out.println("Contact Updated Successfully");
+				}
+			}
+		} else if(memory.toLowerCase().equals("phone")) {
+			for(int i=0; i<sim.size(); i++) {
+				if(phone.get(i).equals(found)) {
+					phone.set(i, updated);
+					System.out.println("Contact Updated Successfully");
+				}
+			}
+		}
+	}
+
+	@Override
+	public Contacts searchContact(String name, String memory) {
+		if(memory.toLowerCase().equals("sim")) {
+			for(int i=0; i<sim.size(); i++) {
+				if(sim.get(i).name.toLowerCase().equals(name)) {
+					return sim.get(i);
+				}
+			}
+			System.out.println("No Contact found with the name: "+name.toUpperCase());
+			return null;
+		} else if(memory.toLowerCase().equals("phone")) {
+			for(int i=0; i<phone.size(); i++) {
+				if(phone.get(i).name.toLowerCase().equals(name)) {
+					return phone.get(i);
+				}
+			}
+			System.out.println("No Contact found with the name: "+name.toUpperCase());
+			return null;
+		}
+		return null;
+	}
+
+	@Override
+	public Contacts searchContact(long mobileNumber, String memory) {
+		if(memory.toLowerCase().equals("sim")) {
+			for(int i=0; i<sim.size(); i++) {
+				if(sim.get(i).mobileNumber == mobileNumber) {
+					return sim.get(i);
+				}
+			}
+			return null;
+		} else if(memory.toLowerCase().equals("phone")) {
+			for(int i=0; i<phone.size(); i++) {
+				if(phone.get(i).mobileNumber == mobileNumber) {
+					return phone.get(i);
+				}
+			}
+			return null;
+		}
+		return null;
+	}
+
+	@Override
+	public void display(ArrayList<Contacts> sim, ArrayList<Contacts> phone) {
+		ArrayList<Contacts> both = new ArrayList<Contacts>();
+		Collections.copy(both, sim);
+		Collections.copy(both, phone);
+		Map<String, Integer> all = new HashMap<String, Integer>();
+		for(int i=0; i<both.size(); i++) {
+			all.put(both.get(i).name, i );
+		}
+		for(int i=0; i<both.size(); i++) {
+			if(i==all.get(both.get(i).name)) {
+				System.out.println(i+1+" "+both.get(i));
+			}
+		}
+	}
+
+	@Override
+	public void copy(Contacts contact, String target) {
+		if(target.equalsIgnoreCase("sim")) {
+			if(!sim.contains(contact)) {
+				sim.add(contact);
+			}
+		} else if(target.equalsIgnoreCase("phone")) {
+			if(!phone.contains(contact)) {
+				phone.add(contact);
+			}
+		}
+		System.out.println("Contact Copied Successfully");
+	}
+
+	@Override
+	public void copyAll(ArrayList<Contacts> contact, String target) {
+		if(target.equalsIgnoreCase("sim")) {
+			for(int i=0; i<contact.size(); i++) {
+				if(!sim.contains(contact.get(i))) {
+					sim.add(contact.get(i));
+				}
+			}
+		} else if(target.equalsIgnoreCase("phone")) {
+			for(int i=0; i<contact.size(); i++) {
+				if(!phone.contains(contact.get(i))) {
+					phone.add(contact.get(i));
+				}
+			}
+		}
+		System.out.println("Contacts Copied Successfully");
+	}	
+}
